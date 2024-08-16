0ab30bce14628d893cf14a38f3dfa637db28d97a
amirh <a_h_berenji@yahoo.com>
2024-08-16 15:55:20 +0330

    including AE class

--- dual_dashboard.py
+++ dual_dashboard.py
@@ -16,6 +16,29 @@ import torch
 from sklearn.metrics import mean_squared_error
 from scipy import signal
 
+class AE(torch.nn.Module):
+  def __init__(self):
+    super(AE, self).__init__()
+    self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(2048, 1024),
+            torch.nn.Tanh(),
+            torch.nn.Linear(1024, 512),
+            torch.nn.Tanh(),
+            torch.nn.Linear(512, 256),
+            torch.nn.Tanh(),
+        )
+    self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(256, 512),
+            torch.nn.Tanh(),
+            torch.nn.Linear(512, 1024),
+            torch.nn.Tanh(),
+            torch.nn.Linear(1024, 2048),
+            torch.nn.Tanh(),
+        )
+  def forward(self, x):
+          encoded = self.encoder(x)
+          decoded = self.decoder(encoded)
+          return decoded
 
 global f
 f = np.linspace(0, 1000, 2048)
